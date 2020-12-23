c30cb78b5c84a29db674259d582c4c5c1b41a8e1
gabriel7mendes <andrewsseven@gmail.com>
2020-12-23 16:59:55 -0300

    react

--- react/exercicios/src/componentes/Multiplos.jsx
+++ react/exercicios/src/componentes/Multiplos.jsx
@@ -4,4 +4,4 @@ export const BoaTarde = props => <h1>Boa tarde {props.nome}!</h1>
 
 export const BoaNoite = props => <h1>Boa noite {props.nome}!</h1>
 
-export default BoaTarde
+export default { BoaTarde, BoaNoite } 
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import BoaTarde, { BoaNoite } from './componentes/Multiplos'
+import Multi, { BoaNoite } from './componentes/Multiplos'
 
 ReactDOM.render(
     <div>
-        <BoaTarde nome="Ana" />
+        <Multi.BoaTarde nome="Ana" />
         <BoaNoite nome="Bia" />
     </div>, 
     document.getElementById('root'))
