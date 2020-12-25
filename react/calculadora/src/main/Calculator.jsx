fe9a3fa02762d02baf98ac3ded9d4310129441d3
gabriel7mendes <andrewsseven@gmail.com>
2020-12-25 15:51:05 -0300

    react

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -24,11 +24,33 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        this.setState({ ...this.initialState })
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if(this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '=' 
+            const currentOperation = this.state.operation 
+
+            const values = [ ...this.state.values ]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e){
+                values[0] = this.state.values[0]
+            }
+
+            values[1] = 0
+
+            this.setState({
+              displayValue: values[0],
+              operation: equals ? null : operation,
+              current: equals ? 0 : 1,
+              clearDisplay: !equals,
+              values
+            })
+        }
     }
 
     addDigit(n) {
@@ -68,7 +90,7 @@ export default class Calculator extends Component {
                 <Button label="1" click={this.addDigit} />
                 <Button label="2" click={this.addDigit} />
                 <Button label="3" click={this.addDigit} />
-                <Button label="+" click={this.addDigit} />
+                <Button label="+" click={this.setOperation} operation />
                 <Button label="0" click={this.addDigit} double />
                 <Button label="." click={this.addDigit} />
                 <Button label="=" click={this.setOperation} operation />
