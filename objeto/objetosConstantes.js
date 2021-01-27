bb84c252da99f0cc63eaae5c0a8bac32168113f4
gabriel7mendes <andrewsseven@gmail.com>
2021-01-27 15:37:28 -0300

    objects

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,19 @@
+const pessoa = {nome: 'Joao'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// da erro
+// pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({nome: 'Joao'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
