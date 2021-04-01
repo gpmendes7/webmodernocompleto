9d03b0705ec074a2083d3329df1dbb94cdae6be4
gabriel7mendes <andrewsseven@gmail.com>
2021-04-01 14:01:55 -0300

    nodejs

--- node/projeto/src/bancoDeDados.js
+++ node/projeto/src/bancoDeDados.js
@@ -2,7 +2,7 @@ const { upperFirst } = require("lodash")
 
 const sequence = {
     _id: 1,
-    get id() { return this.__id++ }
+    get id() { return this._id++ }
 }
 
 const produtos = {}
@@ -21,4 +21,10 @@ function getProdutos() {
     return Object.values(produtos)
 }
 
-module.exports = { salvarProduto, getProduto, getProdutos }
+function excluirProduto(id) {
+    const produto = produtos[id]
+    delete produtos[id]
+    return produto
+}
+
+module.exports = { salvarProduto, getProduto, getProdutos, excluirProduto }
--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,7 +2,10 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancoDeDados.getProdutos())
@@ -14,12 +17,25 @@ app.get('/produtos/:id', (req, res, next) => {
 
 app.post('/produtos/', (req, res, next) => {
     const produto = bancoDeDados.salvarProduto({
-        nome: req.body.name,
+        nome: req.body.nome,
         preco: req.body.preco
     })
     res.send(produto)
 })
 
+app.put('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.delete('/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
+})
 
 app.listen(porta, () => {
     console.log(`Servidor está executando na porta ${porta}.`)
